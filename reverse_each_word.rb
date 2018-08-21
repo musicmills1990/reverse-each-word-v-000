828dffa80de2f25a4501dc94aa5611f3507b8d65
Marc Mills <mdmills1990@gmail.com>
2018-08-21 21:03:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,14 @@
 #split makes the string into an array
 # split the string into an array, then each interpolate so that each word is reversed, then join it back into a string again.
+def reverse_each_word(string)
+  array = string.split " "
+  reversed_array = []
+  array.each do |word|
+  reversed_array << word.reverse
+  reversed_array
+  end
+  reversed_array.join " "
+end
 
 def reverse_each_word(string)
   array = string.split " "
